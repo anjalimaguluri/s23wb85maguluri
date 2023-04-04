0a0df3c7825703549e2ee4a9a9bfe0f8389204cb
anjalimaguluri <magulurinaganjali@gmail.com>
2023-04-04 14:03:19 -0500

    changes made by me

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/employee', employeeRouter);
 app.use('/board', boardRouter);
-app.use('/board', selectorRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
